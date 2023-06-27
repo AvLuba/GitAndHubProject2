59e018d6d0789d4825cbb335f6e60f27becb88fb
LubaAvakova <43254709+LubaAvakova@users.noreply.github.com>
2023-06-27 10:32:59 -0700

    made some changes

---
+++ src/car/Ford.java
@@ -0,0 +1,8 @@
+package car;
+
+public class Ford {
+
+    public void method1(){
+        System.out.println("hello hello");
+    }
+}
--- src/house/SingleFamily.java
+++ src/house/SingleFamily.java
@@ -2,6 +2,6 @@ package house;
 
 public class SingleFamily {
     public void method1(){
-        System.out.println("Hello again");
+        System.out.println("Changed the text");
     }
 }
